e5d8ecbc9b985eeb4120c5f1ef8c0deb82beb0d9
Vipul0052 <vipulsharm969@gmail.com>
2026-03-30 00:49:26 +0530

    optimize build for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-charts': ['recharts'],
+          'vendor-ui': ['motion/react', 'lucide-react'],
+          'vendor-core': ['react', 'react-dom', 'react-router-dom', '@supabase/supabase-js'],
+        }
+      }
+    }
+  }
 })
